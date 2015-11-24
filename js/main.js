defc2e09b3d3b9cfa1b355a0a049940b9154248d
quintons <quintons@qsheppard.com>
2015-11-24 10:48:35 +0000

    committing main js basic structure

--- js/main.js
+++ js/main.js
@@ -15,3 +15,87 @@ $(document).ready(function(){
     $('#jsonInput').val(jsonStr);
   });
 });
+
+//{
+//  "@context": "http://schema.org/",
+//    "@type": "Product",
+//    "name": "Executive Anvil",
+//    "image": "http://www.example.com/anvil_executive.jpg",
+//    "description": "Sleeker than ACME's Classic Anvil, the Executive Anvil is perfect for the business traveler looking for something to drop from a height.",
+//    "mpn": "925872",
+//    "brand": {
+//  "@type": "Thing",
+//      "name": "ACME"
+//},
+//  "aggregateRating": {
+//  "@type": "AggregateRating",
+//      "ratingValue": "4.4",
+//      "reviewCount": "89"
+//},
+//  "offers": {
+//  "@type": "Offer",
+//      "priceCurrency": "USD",
+//      "price": "119.99",
+//      "priceValidUntil": "2020-11-05",
+//      "itemCondition": "http://schema.org/UsedCondition",
+//      "availability": "http://schema.org/InStock",
+//      "seller": {
+//    "@type": "Organization",
+//        "name": "Executive Objects"
+//  }
+//}
+}
+
+var hackday = hackday || {};
+
+;(function(doc, win, $){
+
+  // private members
+var hackday = (function(){
+  var methods = {
+    /**
+     *
+     */
+    validateFields: function(){
+
+    },
+    /**
+     *
+     */
+    getJason: function(){
+
+    },
+    /**
+     *
+     */
+    populateHiddenFields: function(){
+      // get field values
+      var form = 'jsonForm';
+
+      var inputs = $(form).find('input').not('input[type="hidden"]');
+
+      input.each(function(){
+        var $this = this;
+
+      })
+
+    }
+  };
+
+  // public methods
+  var api = {
+    init: function(){
+      return;
+    }
+  }
+
+
+  return {
+    init: init
+  }
+
+})()
+
+})(document, window, $);
+
+
