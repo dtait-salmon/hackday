4c24729cbecf1a2bdad27c6cfcb692f1edd797c9
user1 <user1>
2015-11-25 13:56:50 +0000

    split render functions in to a new js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  $.getJSON("code/requestItems.php", function(result){
-      $.each(result, function(i){
-        $('#urlList').append('<li><a data-toggle="modal" data-target="#jsonModal" href="#" data-value="'+ result[i].jsonld +'">'+ result[i].url +'</a></li>');
-      });
-  });
+
 
   $('#urlList').on('click', 'a', function(e){
     e.preventDefault();
