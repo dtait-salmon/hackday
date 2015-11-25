f2ba89d75cbf5c1233af9e713a9d68fe64fb43d5
user1 <user1>
2015-11-25 12:37:01 +0000

    gitignore

---
+++ clientInsert/seo.js
@@ -0,0 +1,34 @@
+var schemaTools = {};
+
+schemaTools.controller = {
+  init:function(){
+    this.requestSchema();
+  },
+  requestSchema: function(){
+    self = this;
+
+    jQuery.ajax({
+      method: "POST",
+      url: "http://127.0.0.1:8888/schematools/hackday/code/requestJSON.php",
+      data: { url:  window.location.toString().replace(':8888', '') }
+    }).done(function(response) {
+      self.renderSchema(response);
+    });
+  },
+  renderSchema: function(response){
+    // parsedJSON = jQuery.parseJSON(response);
+
+    var linkingEl = document.createElement("script");
+    var linkingData = document.createTextNode(response);
+    var docHead = document.getElementsByTagName("head");
+
+        linkingEl.type = 'application/ld+json';
+        linkingEl.id = 'linkingData'
+        linkingEl.appendChild(linkingData);
+
+        docHead[0].appendChild(linkingEl);
+  }
+};
+jQuery(document).ready(function(){
+  schemaTools.controller.init();
+});
